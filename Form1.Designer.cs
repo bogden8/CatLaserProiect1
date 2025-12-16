f8984400718986269e87517cf30f85cc8e3c620b
bogden08 <muscubogdan463@yahoo.com>
2025-12-16 23:24:49 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CatLaserProiect1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarX = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBarY = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstMiscari = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // trackBarX
+            // 
+            this.trackBarX.Location = new System.Drawing.Point(485, 107);
+            this.trackBarX.Maximum = 180;
+            this.trackBarX.Minimum = 90;
+            this.trackBarX.Name = "trackBarX";
+            this.trackBarX.Size = new System.Drawing.Size(104, 56);
+            this.trackBarX.TabIndex = 2;
+            this.trackBarX.Value = 100;
+            this.trackBarX.Scroll += new System.EventHandler(this.trackBarX_Scroll_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Axa X (Sus-Jos)";
+            // 
+            // trackBarY
+            // 
+            this.trackBarY.Location = new System.Drawing.Point(485, 189);
+            this.trackBarY.Maximum = 180;
+            this.trackBarY.Name = "trackBarY";
+            this.trackBarY.Size = new System.Drawing.Size(104, 56);
+            this.trackBarY.TabIndex = 4;
+            this.trackBarY.Value = 90;
+            this.trackBarY.Scroll += new System.EventHandler(this.trackBarY_Scroll_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(469, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Axa Y (Stanga-Dreapta)";
+            // 
+            // lstMiscari
+            // 
+            this.lstMiscari.FormattingEnabled = true;
+            this.lstMiscari.ItemHeight = 16;
+            this.lstMiscari.Location = new System.Drawing.Point(27, 15);
+            this.lstMiscari.Name = "lstMiscari";
+            this.lstMiscari.Size = new System.Drawing.Size(295, 196);
+            this.lstMiscari.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(27, 217);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(295, 28);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Adaugă Poziția Curentă";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(27, 285);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(295, 28);
+            this.btnSort.TabIndex = 8;
+            this.btnSort.Text = "Sortează  (Algortim)";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(472, 353);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(295, 28);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Salvează în Fișier";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click_1);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(27, 353);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(295, 28);
+            this.btnExecute.TabIndex = 10;
+            this.btnExecute.Text = "Execută Lista";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click_1);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(512, 14);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(130, 23);
+            this.btnConnect.TabIndex = 11;
+            this.btnConnect.Text = "Conectare";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(406, 15);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 22);
+            this.txtPort.TabIndex = 12;
+            this.txtPort.Text = "COM4";
+            this.txtPort.TextChanged += new System.EventHandler(this.txtPort_TextChanged_1);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(27, 251);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(295, 28);
+            this.btnRandom.TabIndex = 13;
+            this.btnRandom.Text = "Generează Mișcări Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(27, 319);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(295, 28);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Curată Listă";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(27, 387);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(295, 28);
+            this.btnStop.TabIndex = 15;
+            this.btnStop.Text = "Stop Execuție";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(472, 387);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(295, 28);
+            this.btnLoad.TabIndex = 16;
+            this.btnLoad.Text = "Incarcă Fișier";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstMiscari);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBarY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarX);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBarY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstMiscari;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;//biblioteca pentru usb
+using System.Threading;
+using System.Threading.Tasks;//sa pot opri executia
+using System.Windows.Forms;
+
+namespace CatLaserProiect1
+{
+    
+    public partial class Form1 : Form
+    {
+        //variabile globale
+        SerialPort portArduino;
+        List<Miscare> listaDeMiscari = new List<Miscare>();
+
+        //generator de numere random
+        Random rng = new Random();
+
+        //variabile de control pt oprire
+        bool oprireSolicitata = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            portArduino = new SerialPort();
+        }
+
+        //functia ce trimite comanda la arduino
+        private void TrimiteLaArduino(int x, int y)
+        {
+            if (portArduino.IsOpen)
+            {
+                string comanda = $"X{x}Y{y}\n";
+                portArduino.Write(comanda);
+            }
+        }
+
+        //functie ce improspateaza lista vizuala
+        private void ActualizeazaListaVizuala()
+        {
+            lstMiscari.Items.Clear();
+            foreach (var m in listaDeMiscari)
+            {
+                lstMiscari.Items.Add(m.ToString());
+            }
+        }
+        //conectare la arduino
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!portArduino.IsOpen)
+                {
+                    portArduino.PortName = txtPort.Text.Trim();
+                    portArduino.BaudRate = 9600;
+                    portArduino.Open();
+                    MessageBox.Show("Conectat cu succes la Arduino!", "Succes");
+                    btnConnect.Text = "Deconectează";
+                }
+                else
+                {
+                    portArduino.Close();
+                    btnConnect.Text = "Conectare";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la conectare: " + ex.Message + "\nVerifică dacă ai scris corect COM-ul!");
+            }
+        }
+
+        //slider X
+        private void trackBarX_Scroll_1(object sender, EventArgs e)
+        {
+            TrimiteLaArduino(trackBarX.Value, trackBarY.Value);
+        }
+
+        //slider Y
+        private void trackBarY_Scroll_1(object sender, EventArgs e)
+        {
+            TrimiteLaArduino(trackBarX.Value, trackBarY.Value);
+        }
+
+        //adaug miscarea in lista
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            Miscare miscareNoua = new Miscare(trackBarX.Value, trackBarY.Value);
+            listaDeMiscari.Add(miscareNoua);
+            ActualizeazaListaVizuala();
+        }
+
+        //algoritmul de sortare bubble sort dupa axa X
+        private void btnSort_Click_1(object sender, EventArgs e)
+        {
+            bool swap;
+            do
+            {
+                swap = false;
+                for (int i = 0; i < listaDeMiscari.Count - 1; i++)
+                {
+                    if (listaDeMiscari[i].X > listaDeMiscari[i + 1].X)
+                    {
+                        Miscare temp = listaDeMiscari[i];
+                        listaDeMiscari[i] = listaDeMiscari[i + 1];
+                        listaDeMiscari[i + 1] = temp;
+                        swap = true;
+                    }
+                }
+            } while (swap);
+
+            ActualizeazaListaVizuala();
+            MessageBox.Show("Lista a fost sortată după axa X!", "Algoritm Finalizat");
+        }
+
+        //salvarae de fisiere
+        private void btnSave_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                //fereastra ce apare cand salvezi
+                SaveFileDialog fereastraSalvare = new SaveFileDialog();
+                fereastraSalvare.Filter = "Text Files|*.txt";
+                fereastraSalvare.Title = "Salvează Mișcările (ex: Sufragerie)";
+                fereastraSalvare.FileName = "camera_mea.txt";
+
+                
+                if (fereastraSalvare.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter sw = new StreamWriter(fereastraSalvare.FileName))
+                    {
+                        foreach (var m in listaDeMiscari)
+                        {
+                            sw.WriteLine($"{m.X},{m.Y}");
+                        }
+                    }
+                    MessageBox.Show("Fișier salvat: " + Path.GetFileName(fereastraSalvare.FileName));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nu am putut salva: " + ex.Message);
+            }
+        }
+
+        //executarea listei
+        private async void btnExecute_Click_1(object sender, EventArgs e)
+        {
+            if (listaDeMiscari.Count == 0)
+            {
+                MessageBox.Show("Lista e goală! Adaugă mișcări sau generează Random.");
+                return;
+            }
+
+            oprireSolicitata = false;
+            btnExecute.Enabled = false;
+
+            foreach (var m in listaDeMiscari)
+            {
+                if (oprireSolicitata == true)
+                {
+                    MessageBox.Show("Execuție oprită de utilizator!");
+                    break;
+                }
+
+                lstMiscari.SelectedIndex = listaDeMiscari.IndexOf(m);
+                lstMiscari.Update();
+
+                TrimiteLaArduino(m.X, m.Y);
+                await Task.Delay(500);
+            }
+
+            if (!oprireSolicitata)
+            {
+                MessageBox.Show("Execuție terminată!");
+            }
+
+            btnExecute.Enabled = true;
+        }
+
+        //generare miscari random
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int randX = rng.Next(90, 170);
+                int randY = rng.Next(0, 160);
+                listaDeMiscari.Add(new Miscare(randX, randY));
+            }
+            ActualizeazaListaVizuala();
+            MessageBox.Show("Am adăugat 10 mișcări aleatorii!");
+        }
+
+        //curatarea listei
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listaDeMiscari.Clear();
+            ActualizeazaListaVizuala();
+            MessageBox.Show("Lista a fost ștearsă!");
+        }
+
+        //butonul de stop
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            oprireSolicitata = true;
+        }
+
+        //incarcarea fisierului in programul meu
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog fereastraIncarcare = new OpenFileDialog();
+                fereastraIncarcare.Filter = "Text Files|*.txt";
+                fereastraIncarcare.Title = "Alege fișierul de încărcat";
+
+                if (fereastraIncarcare.ShowDialog() == DialogResult.OK)
+                {
+                    //citieste dtoate linile din fisier
+                    string[] linii = File.ReadAllLines(fereastraIncarcare.FileName);
+
+                    //stergem lista ca sa nu amestecam camerele
+                    listaDeMiscari.Clear();
+
+                    foreach (string linie in linii)
+                    {
+                        string[] parti = linie.Split(',');//spargem virgula
+                        if (parti.Length == 2)
+                        {
+                            int x = int.Parse(parti[0]);
+                            int y = int.Parse(parti[1]);
+                            listaDeMiscari.Add(new Miscare(x, y));
+                        }
+                    }
+                    ActualizeazaListaVizuala();
+                    MessageBox.Show("Am încărcat cu succes: " + Path.GetFileName(fereastraIncarcare.FileName));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la citire: " + ex.Message);
+            }
+        }
+
+        //Functii vechi generate automat ca sa nu mai dea eroare programul
+        private void txtPort_TextChanged(object sender, EventArgs e) { }
+        private void btnConnect_Click(object sender, EventArgs e) { }
+        private void txtPort_TextChanged_1(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void button1_Click(object sender, EventArgs e) { }
+
+    } 
+
+    
+    public class Miscare
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Miscare(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"Pozitie -> X: {X} grad, Y: {Y} grad";
+        }
+    }
+}
